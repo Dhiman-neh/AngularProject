7ad5f58f4868064bdee47ed0ae965c18b91dad57
Neha Dhiman <nehadhiman.softprodigy@gmail.com>
2022-01-08 13:56:07 +0530

    task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fakeapi';
+  getData: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchgetData();
+  }
+
+  fetchgetData() {
+    this.http.get("https://jsonplaceholder.typicode.com/posts").subscribe((res: any) => {
+      this.getData = res;
+    })
+
+  }
 }
